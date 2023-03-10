0a7a12bab1fa3e15dd043b85d699a6eab5572a27
Denis Leonenko <leonenko.d31@gmail.com>
2023-03-10 19:16:40 +0300

    Project packed with docker

--- API/TestPoint.WebAPI/Program.cs
+++ API/TestPoint.WebAPI/Program.cs
@@ -18,7 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>
 (
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSqlConnection"))
 );
 
 #endregion
