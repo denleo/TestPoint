081143027383207a9ff92ea758bc09039b024f95
Denis Leonenko <leonenko.d31@gmail.com>
2023-04-08 14:06:46 +0300

    Redundant database project removed

--- Database/Tables/Administrator.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Administrator]
-(
-	[AdministratorId] INT NOT NULL IDENTITY(1,1),
-	[LoginId] INT NOT NULL,
-	[updated_at] DATETIME2 NULL
-
-	CONSTRAINT PK_Administrator_AdministratorId PRIMARY KEY (AdministratorId),
-	CONSTRAINT FK_Administrator_Login FOREIGN KEY (LoginId) REFERENCES [Login](LoginId),
-)
