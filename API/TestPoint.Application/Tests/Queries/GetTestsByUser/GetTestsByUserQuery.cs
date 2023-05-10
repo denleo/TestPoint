32cf0e8386e9eb902bff70d76fcbe9be4ff1dbe0
Denis Leonenko <leonenko.d31@gmail.com>
2023-05-10 15:51:02 +0300

    Get user tests endpoint implemented

---
+++ API/TestPoint.Application/Tests/Queries/GetTestsByUser/GetTestsByUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TestPoint.Application.Tests.Queries.GetTestsByUser;
+
+public class GetTestsByUserQuery : IRequest<List<TestInformation>>
+{
+    public Guid UserId { get; set; }
+    public UserTestsFilter Filter { get; set; }
+}
--- API/TestPoint.Persistance/Repositories/TestRepository.cs
+++ API/TestPoint.Persistance/Repositories/TestRepository.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TestPoint.Application.Interfaces.Persistence.Repositories;
+using TestPoint.Application.Tests;
 using TestPoint.Domain;
 
 namespace TestPoint.DAL.Repositories;
 
 public class TestRepository : RepositoryBase<Test, Guid>, ITestRepository
 {
+    public async Task<List<TestInformation>> GetTestListByAuthor(Guid authorId)
+    {
+        return await DbSet
+            .Include(x => x.Questions)
+            .Where(x => x.AuthorId == authorId)
+            .Select(t => new TestInformation(t.Id, "to remove", t.Name, t.Difficulty, t.Questions.Count(), t.EstimatedTime))
+            .ToListAsync();
+    }
+
+    public async Task<List<TestInformation>> GetTestListById(params Guid[] ids)
+    {
+        return await DbSet
+            .Include(x => x.Questions)
+            .Where(x => ids.Contains(x.Id))
+            .Select(t => new TestInformation(t.Id, "to remove", t.Name, t.Difficulty, t.Questions.Count(), t.EstimatedTime))
+            .ToListAsync();
+    }
+
     public TestRepository(DbContext context) : base(context) { }
 
     protected override IQueryable<Test> GetInclusions()
