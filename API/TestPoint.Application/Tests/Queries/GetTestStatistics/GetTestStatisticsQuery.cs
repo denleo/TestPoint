e34aefe31e708a59cf4a1616e4a10049633eb53e
Denis Leonenko <leonenko.d31@gmail.com>
2023-05-12 22:50:17 +0300

    Get test statistics endpoint implemented

---
+++ API/TestPoint.Application/Tests/Queries/GetTestStatistics/GetTestStatisticsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TestPoint.Application.Tests.Queries.GetTestStatistics;
+
+public class GetTestStatisticsQuery : IRequest<TestStatistics?>
+{
+    public Guid TestId { get; set; }
+}
